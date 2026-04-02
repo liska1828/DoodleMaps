61ac9a97f3b482f40b280d5a291e572b50d067f8
lisa <yelyzaveta.ushkevych.25@kse.org.ua>
2026-04-02 23:40:03 +0300

    astar

---
+++ Doodle_Maps/AStar.cs
@@ -0,0 +1,91 @@
+namespace Doodle_Maps;
+using Doodle_Maps.MapGeneration;
+
+public class AStar : IPathFinder
+{
+    public (List<Point>, int) FindPath(string[,] map, Point start, Point destination)
+    {
+        var gScore = new Dictionary<Point, int>();
+        var parent = new Dictionary<Point, Point>();
+        var queue = new PriorityQueue<Point, int>();
+
+        gScore[start] = 0;
+        queue.Enqueue(start, Heuristic(start, destination));
+
+        int visited = 0;
+
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+            visited++;
+
+            if (current.Equals(destination))
+                break;
+
+            foreach (var neighbor in GetNeighbors(current, map))
+            {
+                int tentative = gScore[current] + 1;
+
+                if (!gScore.ContainsKey(neighbor) || tentative < gScore[neighbor])
+                {
+                    gScore[neighbor] = tentative;
+                    parent[neighbor] = current;
+
+                    int f = tentative + Heuristic(neighbor, destination);
+                    queue.Enqueue(neighbor, f);
+                }
+            }
+        }
+
+        var path = RestorePath(parent, start, destination);
+        return (path, visited);
+    }
+
+    private int Heuristic(Point a, Point b)
+    {
+        return Math.Abs(a.Row - b.Row) + Math.Abs(a.Column - b.Column);
+    }
+
+    private List<Point> GetNeighbors(Point p, string[,] map)
+    {
+        var neighbors = new List<Point>();
+        int[,] dirs = { {1,0}, {-1,0}, {0,1}, {0,-1} };
+
+        for (int i = 0; i < 4; i++)
+        {
+            int r = p.Row + dirs[i, 0];
+            int c = p.Column + dirs[i, 1];
+
+            if (r >= 0 && c >= 0 &&
+                r < map.GetLength(0) &&
+                c < map.GetLength(1) &&
+                map[r, c] != "█")
+            {
+                neighbors.Add(new Point(r, c));
+            }
+        }
+
+        return neighbors;
+    }
+
+    private List<Point> RestorePath(Dictionary<Point, Point> parent, Point start, Point end)
+    {
+        var path = new List<Point>();
+
+        if (!parent.ContainsKey(end))
+            return path;
+
+        var current = end;
+
+        while (!current.Equals(start))
+        {
+            path.Add(current);
+            current = parent[current];
+        }
+
+        path.Add(start);
+        path.Reverse();
+
+        return path;
+    }
+}
